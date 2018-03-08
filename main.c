24f8d9ef9e045b7ca4f4ec3a2a357c733c9e552a
Gnxela <qawswedfrgtt@gmail.com>
2018-03-08 14:57:17 +0000

    Fixed pointer problem and extended play.

--- main.c
+++ main.c
@@ -22,20 +22,22 @@ typedef struct Player {
 
 bool requestInput(int inputLength, char output[]);
 void printBoard(Token board[8][8]);
-void setup(Token board[8][8], Player player1, Player player2);
-void play(Token board[8][8], Player player1, Player player2);
+void setup(Token board[8][8], Player *player1, Player *player2);
+void play(Token board[8][8], Player *player1, Player *player2);
 
 int main() {
 	Token board[8][8];
 	Player player1;
 	Player player2;
 	
-	setup(board, player1, player2);
-	play(board, player1, player2);
+	setup(board, &player1, &player2);
+	play(board, &player1, &player2);
 }
 
 void printBoard(Token board[8][8]) {
+	printf("  1 2 3 4 5 6 7 8\n");
 	for (int i = 0; i < 8; i++) {
+		printf("%d ", i + 1);
 		for (int j = 0; j < 8; j++) {
 			if (board[i][j].type == 0) {
 				printf("X ");
@@ -49,7 +51,7 @@ void printBoard(Token board[8][8]) {
  	}
 }
 
-void setup(Token board[8][8], Player player1, Player player2){
+void setup(Token board[8][8], Player *player1, Player *player2){
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
 			board[i][j].type = 10;	
@@ -67,18 +69,18 @@ void setup(Token board[8][8], Player player1, Player player2){
 		printf("Invalid input!\n");
 		printf("Enter Player 1's name: ");
 	}
-	strcpy(player1.name, input);
-	player1.colour = true;
-	player1.tokensPlaced = 0;
+	strcpy(player1->name, input);
+	player1->colour = true;
+	player1->tokensPlaced = 2;
 	
 	printf("Enter Player 2's name: ");
 	while(!requestInput(20, input)) {
 		printf("Invalid input!\n");
 		printf("Enter Player 2's name: ");
 	}
-	strcpy(player2.name, input);
-	player2.colour = false;
-	player2.tokensPlaced = 0;
+	strcpy(player2->name, input);
+	player2->colour = false;
+	player2->tokensPlaced = 2;
 }
 
 /**
@@ -108,6 +110,17 @@ bool requestInput(int inputLength, char output[]) {
     return false;
 }
 
-void play(Token board[8][8], Player player1, Player player2){
-	printBoard(board);
+void play(Token board[8][8], Player *player1, Player *player2) {
+	bool turn = false;
+	while (true) {
+		turn = !turn;
+		Player currentPlayer = turn ? *player1 : *player2;
+		printBoard(board);
+		printf("%s's turn.\n", currentPlayer.name);
+		printf("Tokens remaining: %d\n", 32 - currentPlayer.tokensPlaced);
+
+		char input[20];
+		printf("Enter input to continue.");
+		requestInput(20, input);
+	}
 }
