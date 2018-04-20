833edf6064785a0b1333ebb2701e87d2eb5cbdc3
Gnxela <qawswedfrgtt@gmail.com>
2018-04-20 11:13:29 +0100

    Spaces changed to tabs

--- input.c
+++ input.c
@@ -12,21 +12,21 @@
  *  - output: The inputted line is returned through this array.
  */
 bool requestInput(int inputLength, char output[]) {
-    if (fgets(output, inputLength, stdin)) {
-        if (output[0] == '\n' || output[0] == '\0') {
-            return false;
-        } else {
-            for (int i = 0; i < inputLength; i++) {//Loop through the list and find the first return carriage. Replace this with the null terminator.
-                if (output[i] == '\n') {
-                    output[i] = '\0';
-                    break;
-                }
-            }
-            return true;
-        }
-    } else {
-        printf("We encountered an error reading input.");
-        exit(1);
-    }
-    return false;
+	if (fgets(output, inputLength, stdin)) {
+		if (output[0] == '\n' || output[0] == '\0') {
+			return false;
+		} else {
+			for (int i = 0; i < inputLength; i++) {//Loop through the list and find the first return carriage. Replace this with the null terminator.
+				if (output[i] == '\n') {
+					output[i] = '\0';
+					break;
+				}
+			}
+			return true;
+		}
+	} else {
+		printf("We encountered an error reading input.");
+		exit(1);
+	}
+	return false;
 }
